d0abc865c3ccfbdb28d08091b4e43e10a1f2de54
bjusufbe <bakir@atlantbh.com>
2012-02-16 11:22:43 +0100

    Adding new JMeter component Adding dependencies in pom for hbase and hadoop

---
+++ src/main/java/com/atlantbh/jmeter/plugins/hadooputilities/hdfsoperations/HdfsOperationsGui.java
@@ -0,0 +1,101 @@
+package com.atlantbh.jmeter.plugins.hadooputilities.hdfsoperations;
+
+import java.awt.BorderLayout;
+import javax.swing.BorderFactory;
+import org.apache.jmeter.gui.util.VerticalPanel;
+import org.apache.jmeter.samplers.gui.AbstractSamplerGui;
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jorphan.gui.JLabeledTextField;
+
+/**
+ * Java class representing GUI for the HDFS Operations component in JMeter
+ * 
+ * @author Bakir Jusufbegovic / AtlantBH
+ */
+public class HdfsOperationsGui extends AbstractSamplerGui {
+
+	private static final long serialVersionUID = 1L;
+	
+	private JLabeledTextField inputFilePathTextField = null;
+	private JLabeledTextField outputFilePathTextField = null;
+	private JLabeledTextField nameNodeTextField = null;
+
+	public HdfsOperationsGui()
+	{
+		super();
+		init();
+	}
+	
+	public void init()
+	{
+		setLayout(new BorderLayout());
+        setBorder(makeBorder());
+        add(makeTitlePanel(), BorderLayout.NORTH);
+        
+        VerticalPanel panel = new VerticalPanel();
+        panel.setBorder(BorderFactory.createEtchedBorder());
+        nameNodeTextField = new JLabeledTextField("Namenode");
+        inputFilePathTextField = new JLabeledTextField("Input file destination");
+        outputFilePathTextField = new JLabeledTextField("Output destination on HDFS");
+        
+        panel.add(nameNodeTextField);
+        panel.add(inputFilePathTextField);
+        panel.add(outputFilePathTextField);
+        add(panel, BorderLayout.CENTER);
+	}
+	
+	@Override
+	public void clearGui()
+	{
+	    super.clearGui();
+	    nameNodeTextField.setText("");
+	    inputFilePathTextField.setText("");
+	    outputFilePathTextField.setText("");
+	}
+	
+	@Override
+	public TestElement createTestElement() {
+		// TODO Auto-generated method stub
+		HdfsOperations operations = new HdfsOperations();
+		modifyTestElement(operations);
+		return operations;
+	}
+
+	@Override
+	public String getLabelResource() {
+		return "HDFS operations";
+	}
+	
+	@Override
+	public String getStaticLabel() {
+	    return "HDFS operations";
+	}
+
+	@Override
+	public void modifyTestElement(TestElement element) {
+		// TODO Auto-generated method stub
+		super.configureTestElement(element);
+		if (element instanceof HdfsOperations)
+		{
+			HdfsOperations operations = (HdfsOperations) element;
+			operations.setNameNode(nameNodeTextField.getText());
+			operations.setInputFilePath(inputFilePathTextField.getText());
+			operations.setOutputFilePath(outputFilePathTextField.getText());
+		}
+	}
+	
+	@Override
+	public void configure(TestElement element)
+	{
+		super.configure(element);
+		if (element instanceof HdfsOperations)
+		{
+			HdfsOperations operations = (HdfsOperations) element;
+			nameNodeTextField.setText(operations.getNameNode());
+			inputFilePathTextField.setText(operations.getInputFilePath());
+			outputFilePathTextField.setText(operations.getOutputFilePath());
+		}
+	}
+	
+	
+}
